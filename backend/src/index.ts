0879dbe680fed17fb060cd106d91d8653cd4892c
HsrRaza <hr.raza04@gmail.com>
2026-04-12 22:35:49 +0530

    deployemt fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,14 +8,13 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT
 
-const allowedOrigins = ["https://file-mind-mu.vercel.app", "http://localhost:5173"]
 app.use(cors({
-  origin: allowedOrigins,
+  origin: process.env.FRONTED_URL || "http://localhost:5173333",
   methods: ['POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type']
 }))
 
-import imagePdf from "./src/routes/convert.routes.js"
+import imagePdf from "./routes/convert.routes.js"
 app.use("/api", imagePdf)
 
 
@@ -23,5 +22,4 @@ app.use("/api", imagePdf)
 // Start server
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
-  console.log(`📡 Endpoint: http://localhost:${PORT}/api/data`);
 });
